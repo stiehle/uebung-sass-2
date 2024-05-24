fe5a9f397387efb1f066b9dcec1f91741dc5a556
stiehle <gstiehle@yahoo.de>
2024-05-24 17:48:03 +0200

    third commit

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ import { products, star } from "./data";
 
 export function makeProducts() {
   let html = "";
-  let html1 = "";
-  let html2 = "";
-  let html3 = "";
 
   for (let element of products) {
     console.log(element);
 
-    html1 = `
+    html += `
     <div class="product">
           <div class="product__badge product__badge${element.badge}">${element.badge_text}</div>
           <img src="${element.file}" class="product__image"></img>
@@ -17,23 +14,23 @@ export function makeProducts() {
     `;
 
     if (!element.instock) {
-      html2 = `     
-            <p class="product__stock product__stock--out-of-stock">nicht im Lager</p>           
+      html += `     
+          <div class="product__stock product__stock--out-of-stock">nicht im Lager</div>           
     `;
     } else {
-      html2 = `     
-            <p class="product__stock product__stock--in-stock">im Lager</p>            
+      html += `     
+          <div class="product__stock product__stock--in-stock">im Lager</div>            
     `;
     }
 
-    html2 += `<p class="product__stars">`;
+    html += `<div class="product__stars">`;
 
     for (let i = 0; i < `${element.stars}`; i++) {
-      html2 += `${star}`;
+      html += `${star}`;
     }
 
-    html3 = `       
-            </p>
+    html += `       
+            </div>
           </div>
           <div class="product__description">
             <div class="product__title"><h2>${element.title}</h2></div>
@@ -43,10 +40,8 @@ export function makeProducts() {
             <div class="product__price product__price--reduce">${element.price_reduce}${element.price_unit}</div>
             <div class="product__price product__price--basis">${element.price_basis}${element.price_unit}</div>
           </div>
-        </div>
+      </div>
     `;
-
-    html += html1 + html2 + html3;
   }
   return html;
 }
--- main.js
+++ main.js
@@ -7,6 +7,6 @@ document.querySelector("#app").innerHTML = `
 <section class="product-section">
   <h2 class="product-section__heading">Bestseller und neue Produkte</h2>
   <div class="product-section__product-list">${makeProducts()}</div>
-<a class="product-section__button" href="#">weitere Produkte anzeigen</a>
+  <a class="product-section__button" href="#">weitere Produkte anzeigen</a>
 </section>
 `;
